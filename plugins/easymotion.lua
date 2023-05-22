0ef5f446e6dcaf643292d47278d98636f988093a
Asshurim Joy Larita <a.larita@arcanys.com>
2023-05-22 12:53:58 +0800

    Add easy motion plugin with key mapping

---
+++ plugins/easymotion.lua
@@ -0,0 +1,22 @@
+return {
+  "phaazon/hop.nvim",
+  version = "*", -- Use for stability; omit to use `main` branch for the latest features
+  event = "VeryLazy",
+  config = function()
+    require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+    local hop = require('hop')
+    local directions = require('hop.hint').HintDirection
+    vim.keymap.set('', 'f', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+    end, { remap = true })
+    vim.keymap.set('', 'F', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+    end, { remap = true })
+    vim.keymap.set('', 't', function()
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+    end, { remap = true })
+    vim.keymap.set('', 'T', function()
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+    end, { remap = true })
+  end
+}
