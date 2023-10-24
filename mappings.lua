492f9d86bf698952c20d8937db3ea883c1dd9011
Asshurim Joy Larita <a.larita@arcanys.com>
2023-10-24 20:28:53 +0800

    Add essential test mappings and formatting

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,10 @@
 return {
   n = {
-    ["∑"] = { "<cmd>bw!<cr>", desc = "Destroy current buffer" }
+    ["<leader><leader>x"] = { "<cmd>bw!<cr>", desc = "Destroy current buffer" },
+    ["<leader>x"] = { "<cmd>bw!<cr>", desc = "Destroy current buffer" },
+    ["<leader><leader>e"] = { "<cmd>EslintFixAll<cr>", desc = "Eslint Fix All" },
+    -- ["<leader><leader>t"] = { "<cmd>!w <cr> :vplit term://yarn test<cr> :norm G<cr>", desc = "Run all tests" },
+    ["<leader><leader>c"] = { "<cmd>:only <cr>", desc = "Focus current buffer" },
+    ["<leader><leader>t"] = { "<cmd>:vsplit term://yarn test <cr>", desc = "Run full test suite" },
   }
 }
