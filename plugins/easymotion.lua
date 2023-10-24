5c7056555d2e07616e95dea041ef5c5dd8097f9e
Asshurim Joy Larita <a.larita@arcanys.com>
2023-10-24 20:28:09 +0800

    Use single stroke of key 's' in normal mode for easy motion

--- plugins/easymotion.lua
+++ plugins/easymotion.lua
@@ -6,11 +6,14 @@ return {
     require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
     local hop = require('hop')
     local directions = require('hop.hint').HintDirection
+    vim.keymap.set('', 's', function()
+      hop.hint_char1({ current_line_only = false })
+    end, { remap = true })
     vim.keymap.set('', 'f', function()
-      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = false })
+      hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
     end, { remap = true })
     vim.keymap.set('', 'F', function()
-      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = false })
+      hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
     end, { remap = true })
     vim.keymap.set('', 't', function()
       hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
